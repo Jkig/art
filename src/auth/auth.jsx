5afa4e2aaccd8bc11fb6f4151b86468d1c0f1dfb
Jkig <DerekLeroux47@gmail.com>
2023-02-26 15:01:05 -0700

    pre vite local env. variable changes

--- src/auth/auth.jsx
+++ src/auth/auth.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './login'
+import '../index.css'
+import '../App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/auth/login.jsx
+++ src/auth/login.jsx
@@ -1,9 +1,28 @@
+import { FcGoogle } from "react-icons/fc";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../utils/firebase.js"
+
 export default function Login() {
+    const googleProvider = new GoogleAuthProvider();
+    const GoogleLogin = async () => {
+        try {
+            const result = await signInWithPopup(auth, googleProvider);
+        } catch (error) {
+
+        }
+    }
+
     return (
-        <div>
-            <h2>Join</h2>
-            <div>
-                <button>Sign in with Google</button>
+        <div className="loginPage">
+            <div className="loginBox">
+                <h2 className="centerMe">Join</h2>
+                <div>
+                    <button className="loginButton">
+                        Sign in with Google
+                        <div className="oneSpace"></div>
+                        <FcGoogle className="googleIcon"/>
+                    </button>
+                </div>
             </div>
         </div>
     )
