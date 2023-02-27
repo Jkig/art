fe1adb0b92500e0470e7d47e774cacc01c9befa8
Jkig <DerekLeroux47@gmail.com>
2023-02-27 08:48:16 -0700

    Final before adding react-router

--- src/User.jsx
+++ src/User.jsx
@@ -5,10 +5,8 @@ export default function User(){
                 // if logged in, show log out instead
             }
             <div className="authButtons">
-                <a className="authButton" href="/auth/login.html">Join</a>
-                <div className="spacer" />
-                <a className="authButton" href="/auth/login.html">log In</a>
             </div>
+            <a className="authButton" href="/auth/login.html">log In</a>
             <ul className="unBulleted">
                 <li><a>Profile</a></li>
                 <li><a>Saved</a></li>
