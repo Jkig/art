c12f8e81d403e2dea09de044218562841100bfe3
Jkig <DerekLeroux47@gmail.com>
2023-03-04 19:45:39 -0700

    right before the photo uploading thing

--- src/create.jsx
+++ src/create.jsx
@@ -2,16 +2,24 @@ import { auth, db } from "../utils/firebase"
 import { useAuthState } from "react-firebase-hooks/auth"
 import { useEffect, useState } from "react"
 import { AiOutlineCloseCircle } from "react-icons/Ai";
-// TODO: add photos later... add connections between things later
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+
+
+// I need to upload the photo
+// create reference
+// create post, including reference
+import { getStorage, ref } from "firebase/storage";
+
 
 export default function Create(props){
-    const [post, setPost] = useState({imageSrc: "", title: "", description: ""})//maybe user id too?, probably fine without
-    
-    // stuffs here
-    const [preview, setPreview] = useState()
+    const [post, setPost] = useState({imageSrc: undefined, title: "", description: ""})//maybe user id too?, probably fine without
+    const [preview, setPreview] = useState();
+    const[user,loading] = useAuthState(auth);
+
+
     useEffect(() => {
         if (!post.imageSrc) {
-            setPost({imageSrc: undefined})
+            setPost({...post, imageSrc: undefined})
             return
         }
         const objectUrl = URL.createObjectURL(post.imageSrc)
@@ -19,26 +27,42 @@ export default function Create(props){
         return () => URL.revokeObjectURL(objectUrl)
     }, [post.imageSrc])
 
-    const handleFile = e => {
+
+    const handleFile = async (e) => {
         if (!e.target.files || e.target.files.length === 0) {
-            setPost({imageSrc: undefined})
+            setPost({...post, imageSrc: undefined})
             return
         }
-        setPost({imageSrc: e.target.files[0]})
+        setPost({...post, imageSrc: e.target.files[0]})
     }
-
+    
+    
+    const submitPost = async (e) => {
+        e.preventDefault();
+        const collectionRef = collection(db, "posts");
+        await addDoc(collectionRef, {
+            ...post,
+            //imageSrc: imageRef.fullPath, // just connect the image reference here, but trough the firebase bucket
+            timestamp: serverTimestamp(),
+            user: user.uid,
+            avatar: user.photoURL,
+            username: user.displayName,
+        });
+    }
 
     return (
         <div className="mainCreate">
             <AiOutlineCloseCircle onClick={() => props.handleCreating()} className="closeIcon"/>
-            <form>
+            <form onSubmit={submitPost}>
                 <div className="imageBox">
                     {post.imageSrc ? <img className="imagePreview" src={preview}/> : <h2>No Image</h2>}
                     {!post.imageSrc && <input type="file" onChange={handleFile}/>}
                 </div>
-                <textarea className="title" placeholder="Title" onChange={(e) => setPost({title: e.target.value})} value={post.title} />
-                <textarea className="description" placeholder="Description" onChange={(e) => setPost({description: e.target.value})} value={post.description} />
-                <button className="createButton">Create</button>
+                <textarea className="title" placeholder="Title" onChange={(e) => setPost({...post, title: e.target.value})} value={post.title} />
+                <p className={`lengthIndicator${post.title.length > 100 ? "Red" : ""}`}>{post.title.length}/100</p>
+                <textarea className="description" placeholder="Description" onChange={(e) => setPost({...post, description: e.target.value})} value={post.description} />
+                <p className={`lengthIndicator${post.description.length > 300 ? "Red" : ""}`}>{post.description.length}/300</p>
+                <button className="createButton" type="submit">Create</button>
             </form>
         </div>
     )
--- utils/firebase.js
+++ utils/firebase.js
@@ -24,3 +24,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 export const db = getFirestore(app);
+
